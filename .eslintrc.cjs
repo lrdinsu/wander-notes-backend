c660c96b935eddeb2f2e33762a7d24ab62072d0c
lilia <143701282+lrdinsu@users.noreply.github.com>
2023-12-31 07:45:33 -0800

    feat: Update comfigs, Add new images and update server endpoints

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,14 +10,6 @@ module.exports = {
     node: true,
   },
   rules: {
-    '@typescript-eslint/semi': ['error'],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/restrict-template-expressions': 'off',
-    '@typescript-eslint/restrict-plus-operands': 'off',
-    '@typescript-eslint/no-unsafe-member-access': 'off',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'no-case-declarations': 'off',
     '@typescript-eslint/no-misused-promises': [
       'error',
       {
@@ -26,6 +18,7 @@ module.exports = {
         },
       },
     ],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,57 @@
 import express from 'express';
+import { readFileSync } from 'fs';
+import { writeFile } from 'fs/promises';
+
+import { Tour } from './types/type.js';
 
 const app = express();
 
-app.get('/', (_, res) => {
-  res
-    .status(404)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
+app.use(express.json());
+
+// app.get('/', (_, res) => {
+//   res
+//     .status(404)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (_, res) => {
+//   res.send('You can post to this endpoint...');
+// });
+
+const datapath = new URL('../dev-data/data', import.meta.url).pathname;
+
+const tours = JSON.parse(
+  readFileSync(`${datapath}/tours-simple.json`, 'utf-8'),
+) as Tour[];
+
+app.get('/api/v1/tours', (_, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (_, res) => {
-  res.send('You can post to this endpoint...');
+app.post('/api/v1/tours', async (req, res) => {
+  // console.log(req.body);
+  const newId = tours.length === 0 ? 1 : tours[tours.length - 1].id + 1;
+  const newTour = { ...req.body, id: newId } as Tour;
+
+  tours.push(newTour);
+  await writeFile(`${datapath}/tours-simple.json`, JSON.stringify(tours));
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      our: newTour,
+    },
+  });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT ?? 3000;
 
 app.listen(PORT, () => {
-  console.log(`App runing on port ${PORT}...`);
+  console.log(`App running on port ${PORT}...`);
 });
