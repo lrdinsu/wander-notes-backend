960d7b789f3ae4e494f9d87c3eda581fbc8c565f
lilia <143701282+lrdinsu@users.noreply.github.com>
2024-01-14 03:29:30 -0800

    feat: update error handler middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -15,14 +15,13 @@ export const errorMiddleware: ErrorRequestHandler = (err, _, res, __) => {
     return;
   }
 
-  console.log(err.stack);
-
   const error = err as HttpError;
   const statusCode = error.statusCode || 500;
   const status = error.status || 'error';
   const message = error.message || 'Internal Server Error';
 
-  console.error('💥', message);
+  console.error('💥', error.stack);
+
   res.status(statusCode).json({
     status: status,
     message: `💥, ${message}`,
